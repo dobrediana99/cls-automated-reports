864ad0ba467cc4049dafc58ee850bf1ae8a14b16
dobrediana99 <diana.dobre99@gmail.com>
2026-05-06 08:52:14 +0300

    excluded sora and mocanu

--- backend/config/org.js
+++ backend/config/org.js
@@ -16,14 +16,12 @@ export const ORG = [
   { name: "Denisa Ionescu", email: "denisa.i@crystal-logistics-services.com", mondayUserId: 74108553, department: DEPARTMENTS.SALES, role: "employee", target: 2442, isActive: true },
   { name: "Andrei Pauna", email: "andrei.p@crystal-logistics-services.com", mondayUserId: 73046350, department: DEPARTMENTS.SALES, role: "employee", target: 3400, isActive: true },
   { name: "Nedelcu Alexandru", email: "alexandru.n@crystal-logistics-services.com", mondayUserId: 77987246, department: DEPARTMENTS.SALES, role: "employee", target: 3630, isActive: true },
-  { name: "Christiana Sora", email: "christiana.s@crystal-logistics-services.com", mondayUserId: 90770132, department: DEPARTMENTS.SALES, role: "employee", target: 2310, isActive: true },
   { name: "David Popescu", email: "david.p@crystal-logistics-services.com", mondayUserId: 74695692, department: DEPARTMENTS.OPERATIONS, role: "employee", target: 4007, isActive: true },
   { name: "Roberto Coica", email: "roberto.c@crystal-logistics-services.com", mondayUserId: 74668675, department: DEPARTMENTS.OPERATIONS, role: "employee", target: 3240, isActive: true },
   { name: "Dumitru Ionut", email: "ionut.d@crystal-logistics-services.com", mondayUserId: 74668676, department: DEPARTMENTS.OPERATIONS, role: "employee", target: 3240, isActive: true },
   { name: "Robert Florea", email: "robert.f@crystal-logistics-services.com", mondayUserId: 74668678, department: DEPARTMENTS.OPERATIONS, role: "employee", target: 4590, isActive: true },
   { name: "Alexandra Ghiurca", email: "alexandra.g@crystal-logistics-services.com", mondayUserId: 96280239, department: DEPARTMENTS.OPERATIONS, role: "employee", target: 3040, isActive: true },
   { name: "David Mitrica", email: "david.m@crystal-logistics-services.com", mondayUserId: 89227837, department: DEPARTMENTS.OPERATIONS, role: "employee", target: 1920, isActive: true },
-  { name: "Mocanu George", email: "george.m@crystal-logistics-services.com", mondayUserId: 96568400, department: DEPARTMENTS.OPERATIONS, role: "employee", target: 3040, isActive: true },
 ];
 
 export const MANAGERS = ORG.filter((p) => p.role === "manager" && p.isActive);
